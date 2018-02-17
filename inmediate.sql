838277f0e49dda3c906e28db6136899e13a2431a
Andres Rios <rianby64@gmail.com>
2018-02-17 14:27:18 +0300

    Gotcha! Ya vamos viendo como funciona

--- inmediate.sql
+++ inmediate.sql
@@ -1,3 +1,4 @@
+\set QUIET 1
 
 DROP TABLE IF EXISTS "Aux" CASCADE;
 CREATE TABLE IF NOT EXISTS "Aux"
@@ -9,6 +10,36 @@ CREATE TABLE IF NOT EXISTS "Aux"
 WITH (
   OIDS=FALSE
 );
+
+CREATE OR REPLACE FUNCTION aux_delete_before()
+  RETURNS TRIGGER AS
+$$
+BEGIN
+  RAISE NOTICE 'aux_delete_before %', OLD;
+  RETURN OLD;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER "Aux_delete_before"
+  BEFORE DELETE
+  ON "Aux"
+  FOR EACH ROW
+  EXECUTE PROCEDURE aux_delete_before();
+
+CREATE OR REPLACE FUNCTION aux_delete_after()
+  RETURNS TRIGGER AS
+$$
+BEGIN
+  RAISE NOTICE 'aux_delete_after %', OLD;
+  RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER "Aux_delete_after"
+  AFTER DELETE
+  ON "Aux"
+  FOR EACH ROW
+  EXECUTE PROCEDURE aux_delete_after();
 
 
 DROP TABLE IF EXISTS "Maestra" CASCADE;
@@ -26,6 +57,36 @@ WITH (
   OIDS=FALSE
 );
 
+CREATE OR REPLACE FUNCTION maestra_delete_before()
+  RETURNS TRIGGER AS
+$$
+BEGIN
+  RAISE NOTICE 'maestra_delete_before %', OLD;
+  RETURN OLD;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER "Maestra_delete_before"
+  BEFORE DELETE
+  ON "Maestra"
+  FOR EACH ROW
+  EXECUTE PROCEDURE maestra_delete_before();
+
+CREATE OR REPLACE FUNCTION maestra_delete_after()
+  RETURNS TRIGGER AS
+$$
+BEGIN
+  RAISE NOTICE 'maestra_delete_after %', OLD;
+  RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER "Maestra_delete_after"
+  AFTER DELETE
+  ON "Maestra"
+  FOR EACH ROW
+  EXECUTE PROCEDURE maestra_delete_after();
+
 INSERT INTO "Aux" (msg) VALUES
   ('Aux 1'),
   ('Aux 2'),
@@ -38,12 +99,4 @@ INSERT INTO "Maestra" ("AuxId", msg) VALUES
   (2, 'Maestra 2'),
   (3, 'Maestra 3');
 
-LOAD 'auto_explain';
-set auto_explain.log_analyze = on;
-set auto_explain.log_min_duration = 0;
-set auto_explain.log_triggers = on;
-
-NOTIFY virtual, '{"action":"test-start-log"}';
-
 DELETE FROM "Aux" WHERE id=3;
-NOTIFY virtual, '{"action":"test-end-log"}';
