51661aa7423ee60e10adf51c41f4d4403a7fc234
Andres Rios <rianby64@gmail.com>
2018-02-18 13:36:13 +0300

    Muestre el resultado de cambiar a STABLE

--- inmediate.sql
+++ inmediate.sql
@@ -60,9 +60,13 @@ psql:inmediate.sql:16: NOTICE:  (5,"Aux0Aux0 5") (5,5,"Aux0 5") (5,"Aux1 5") (4,
 psql:inmediate.sql:16: NOTICE:  PERFORM showtables() >
 psql:inmediate.sql:16: NOTICE:  maestra_delete_before (2,2,3,"Maestra 2")
 psql:inmediate.sql:16: NOTICE:  PERFORM showtables() <
+psql:inmediate.sql:16: NOTICE:  Aux0Aux0 = 5 Aux0 = 4 Aux1 = 4 Maestra = 3
+psql:inmediate.sql:16: NOTICE:  (5,"Aux0Aux0 5") (5,5,"Aux0 5") (5,"Aux1 5") (4,4,5,"Maestra 3")
 psql:inmediate.sql:16: NOTICE:  PERFORM showtables() >
 psql:inmediate.sql:16: NOTICE:  maestra_delete_after (2,2,3,"Maestra 2")
 psql:inmediate.sql:16: NOTICE:  PERFORM showtables() <
+psql:inmediate.sql:16: NOTICE:  Aux0Aux0 = 5 Aux0 = 4 Aux1 = 5 Maestra = 3
+psql:inmediate.sql:16: NOTICE:  (5,"Aux0Aux0 5") (5,5,"Aux0 5") (5,"Aux1 5") (4,4,5,"Maestra 3")
 psql:inmediate.sql:16: NOTICE:  PERFORM showtables() >
 psql:inmediate.sql:20: NOTICE:  DELETE FROM "Aux0Aux0" WHERE id=1;
 psql:inmediate.sql:21: NOTICE:  aux0aux0_delete_before (1,"Aux0Aux0 1")
@@ -77,15 +81,24 @@ psql:inmediate.sql:21: NOTICE:  (5,"Aux0Aux0 5") (5,5,"Aux0 5") (5,"Aux1 5") (4,
 psql:inmediate.sql:21: NOTICE:  PERFORM showtables() >
 psql:inmediate.sql:21: NOTICE:  aux0_delete_before (1,1,"Aux0 1")
 psql:inmediate.sql:21: NOTICE:  PERFORM showtables() <
+psql:inmediate.sql:21: NOTICE:  Aux0Aux0 = 4 Aux0 = 4 Aux1 = 4 Maestra = 2
+psql:inmediate.sql:21: NOTICE:  (5,"Aux0Aux0 5") (5,5,"Aux0 5") (5,"Aux1 5") (4,4,5,"Maestra 3")
 psql:inmediate.sql:21: NOTICE:  PERFORM showtables() >
+psql:inmediate.sql:21: NOTICE:  ante_aux0_delete_before (1,1,"Aux0 1")
 psql:inmediate.sql:21: NOTICE:  aux0_delete_after (1,1,"Aux0 1")
 psql:inmediate.sql:21: NOTICE:  PERFORM showtables() <
+psql:inmediate.sql:21: NOTICE:  Aux0Aux0 = 5 Aux0 = 4 Aux1 = 4 Maestra = 2
+psql:inmediate.sql:21: NOTICE:  (5,"Aux0Aux0 5") (5,5,"Aux0 5") (5,"Aux1 5") (4,4,5,"Maestra 3")
 psql:inmediate.sql:21: NOTICE:  PERFORM showtables() >
 psql:inmediate.sql:21: NOTICE:  maestra_delete_before (1,1,2,"Maestra 1")
 psql:inmediate.sql:21: NOTICE:  PERFORM showtables() <
+psql:inmediate.sql:21: NOTICE:  Aux0Aux0 = 4 Aux0 = 3 Aux1 = 4 Maestra = 2
+psql:inmediate.sql:21: NOTICE:  (5,"Aux0Aux0 5") (5,5,"Aux0 5") (5,"Aux1 5") (4,4,5,"Maestra 3")
 psql:inmediate.sql:21: NOTICE:  PERFORM showtables() >
 psql:inmediate.sql:21: NOTICE:  maestra_delete_after (1,1,2,"Maestra 1")
 psql:inmediate.sql:21: NOTICE:  PERFORM showtables() <
+psql:inmediate.sql:21: NOTICE:  Aux0Aux0 = 5 Aux0 = 4 Aux1 = 4 Maestra = 2
+psql:inmediate.sql:21: NOTICE:  (5,"Aux0Aux0 5") (5,5,"Aux0 5") (5,"Aux1 5") (4,4,5,"Maestra 3")
 psql:inmediate.sql:21: NOTICE:  PERFORM showtables() >
 psql:inmediate.sql:25: NOTICE:  version = PostgreSQL 9.6.7 on x86_64-pc-linux-gnu, compiled by gcc (GCC) 4.8.5 20150623 (Red Hat 4.8.5-16), 64-bit
 */
